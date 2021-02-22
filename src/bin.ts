71da2ae56bb0be91fbc23272337b84a80e2d0a81
Joel Denning <joeldenning@gmail.com>
2021-02-22 14:51:54 -0700

    Support for pnpm

--- src/bin.ts
+++ src/bin.ts
@@ -5,10 +5,10 @@ import { hideBin } from 'yargs/helpers'
 import { run } from './index'
 import { error } from './log'
 
-export type PackageManager = 'npm' | 'yarn'
+export type PackageManager = 'npm' | 'yarn' | 'pnpm'
 export type PackageManagers = ReadonlyArray<PackageManager>
 
-const packageManagers: PackageManagers = ['npm', 'yarn']
+const packageManagers: PackageManagers = ['npm', 'yarn', 'pnpm']
 const argv = yargs(hideBin(process.argv)).option('package-manager', {
   alias: 'pm',
   describe: 'package manager to be used',
